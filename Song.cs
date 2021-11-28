77d5ec78dcacdc723812540133bc8a67dd24db0f
Kukuynik <ilya42rs@gmail.com>
2021-11-28 23:30:51 +0700

    Update Song.cs

--- Song.cs
+++ Song.cs
@@ -8,5 +8,47 @@ namespace laba10
 {
     class Song
     {
+        string name;
+        string author;
+        Song prev;
+
+        public string songName
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string songAuthor
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+        public void setPrev(Song song)
+        {
+            prev = song;
+        }
+        public string Title()
+        {
+            return $"{name} - {author}";
+        }
+        public override bool Equals(object d)
+        {
+            if (d.GetType() == GetType())
+                return true;
+            else
+                return false;
+        }
     }
 }
