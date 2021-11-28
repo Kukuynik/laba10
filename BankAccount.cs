e44f01a92b98e94e8d76968d63894801f78a01e4
Kukuynik <ilya42rs@gmail.com>
2021-11-28 18:58:16 +0700

    Initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba10
+{
+    public enum TypeOfAccount
+    {
+        DEBIT,
+        CREDIT,
+        DEPOSIT
+    }
+    public class BankAccount
+    {
+        protected int _number;
+        protected decimal _balance;
+        protected TypeOfAccount _typeAccount;
+        static int _counter = 1;
+        public BankAccount()
+        {
+            _number = Increase();
+        }
+        public BankAccount(decimal balance)
+        {
+            _balance = balance;
+            _number = Increase();
+        }
+        public BankAccount(TypeOfAccount typeAccount)
+        {
+            _typeAccount = typeAccount;
+            _number = Increase();
+        }
+
+        public int Increase()
+        {
+            return _counter++;
+        }
+
+        public int accountNumber
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+        public decimal accountBalance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+        public TypeOfAccount accountType
+        {
+            get { return _typeAccount; }
+            set { _typeAccount = value; }
+        }
+        public BankAccount(decimal balance, TypeOfAccount typeAccount)
+        {
+            _balance = balance;
+            _typeAccount = typeAccount;
+            _number = Increase();
+        }
+        public void TransferMoney(ref BankAccount transferAccaunt, int amount)
+        {
+            if (transferAccaunt.accountBalance >= amount)
+            {
+                transferAccaunt.accountBalance -= amount;
+                this.accountBalance += amount;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba10
+{
+    public partial class Form1 : Form
+    {
+        BankAccount account1 = new BankAccount(3000, TypeOfAccount.DEBIT);
+        BankAccount account2 = new BankAccount(1400, TypeOfAccount.DEBIT);
+        List<BankAccount> listAccoutnt = new List<BankAccount>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add(account1.accountNumber);
+            comboBox1.Items.Add(account2.accountNumber);
+            comboBox2.Items.Add(account1.accountNumber);
+            comboBox2.Items.Add(account2.accountNumber);
+            listAccoutnt.Add(account1);
+            listAccoutnt.Add(account2);
+            richTextBox1.Text = File.ReadAllText("Ex5.txt");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == account1.accountNumber-1)
+            {
+                textBox2.Text = Convert.ToString(account1.accountType);
+                textBox3.Text = Convert.ToString(account1.accountBalance);
+            }
+            else if(comboBox1.SelectedIndex == account2.accountNumber-1)
+            {
+                textBox2.Text = Convert.ToString(account2.accountType);
+                textBox3.Text = Convert.ToString(account2.accountBalance);
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex == account1.accountNumber-1)
+            {                
+                textBox1.Text = Convert.ToString(account1.accountBalance);
+            }
+            else if (comboBox2.SelectedIndex == account2.accountNumber-1)
+            {
+                textBox1.Text = Convert.ToString(account2.accountBalance);
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != comboBox2.SelectedIndex)
+            {
+                switch (comboBox1.Text)
+                {
+                    case "1":
+                        account2.TransferMoney(ref account1, Convert.ToInt32(textBox4.Text));
+                        textBox3.Text = Convert.ToString(account1.accountBalance);
+                        textBox1.Text = Convert.ToString(account2.accountBalance);
+                        break;
+                    case "2":
+                        account1.TransferMoney(ref account2, Convert.ToInt32(textBox4.Text));
+                        textBox3.Text = Convert.ToString(account2.accountBalance);
+                        textBox1.Text = Convert.ToString(account1.accountBalance);
+                        break;
+                }
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != 127)
+            {
+                e.Handled = true;
+            }
+        }
+
+        // zadanie 2 
+        static string Reverse(string str)
+        {
+            char[] reverse = str.ToCharArray();
+            Array.Reverse(reverse);
+            return new string(reverse);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = Reverse(textBox5.Text);
+        }
+        // zadanie 4
+
+        const string outputFileName = "ResultFile.txt";
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox6.Text))
+            {
+                File.WriteAllText(outputFileName, File.ReadAllText(textBox6.Text, Encoding.UTF8).ToUpper());
+                MessageBox.Show("Результат успешно записан в файл");
+            }
+            else
+            {
+                MessageBox.Show($"Файл с именем \"{textBox6.Text}\" не найден!");
+            }
+        }
+        // zadanie 5
+        public void SearchMail(ref string s)
+        {
+            string[] email = s.Split(new char[] { '#' });
+            s = email[1];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("ResultEx5.txt"))
+            {
+                File.Delete("ResultEx5.txt");
+            }
+            richTextBox2.Clear();
+            List<string> filestring = new List<string>();
+            filestring = File.ReadAllLines("Ex5.txt").ToList();
+            for (int count = 0; count < filestring.Count; count++)
+            {
+                string s = filestring[count];
+                SearchMail(ref s);
+                richTextBox2.Text += s+'\r';
+            }
+            File.WriteAllText("ResultEx5.txt", richTextBox2.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
